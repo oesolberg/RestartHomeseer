4c6f51dc7aec2409c29a9b9b974d670dec1c5da0
Odd Erik Midthaug <oesolberg@hotmail.com>
2018-04-05 22:57:56 +0200

    Added working dir to get stuff correctly working

--- CheckIfHomeseerIsRunning.ps1
+++ CheckIfHomeseerIsRunning.ps1
@@ -3,6 +3,6 @@ $programName = "HS3"
 $isRunning = (Get-Process | Where-Object { $_.Name -eq $programName }).Count -gt 0
 
 if ($isRunning -eq $false){
-    Start-Process "C:\Program Files (x86)\HomeSeer HS3\HS3.exe"
+    Start-Process -FilePath "C:\Program Files (x86)\HomeSeer HS3\HS3.exe" -WorkingDirectory "C:\Program Files (x86)\HomeSeer HS3\"
     Write-EventLog -LogName "HomeseerRestart" -Source "HomeseerRestart" -Message "Restarted Homeseer since it was not found" -EventId 9 -EntryType Error
 }
